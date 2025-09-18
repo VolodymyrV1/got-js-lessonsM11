ce7644282df9882c316bcbe5cdcc7f02a928f686
VolodymyrV <kotsiubkov@gmail.com>
2025-09-18 17:34:44 +0300

    1

---
+++ Lesson 22/js/01-axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import '../common.css';
+
+/**
+ * Додаємо та використовуємо бібліотеку https://axios-http.com/
+ * Робимо рефакторинг
+ * - Імпорт
+ * - Запит
+ * - Метод axios.get
+ * Обробка відповіді та помилки
+ */
+
+const url = "https://jsonplaceholder.typicode.com/todos";
